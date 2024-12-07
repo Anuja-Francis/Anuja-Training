25136e610e5e1819c0497aa7d66777b6f6e959f8
Anuja Francis <anuja.francis@aspiresys.kl>
2024-12-07 10:57:21 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+
+// let icecream = "chocolate";
+// if ( icecream == "chocolate" ){
+//     alert("i iove chocolate icecream");
+// }
+// else {
+//     alert("chocolate is my favorite");
+// }
+
+function multiply( num1 , num2 ){
+    let res = num1 * num2;
+    alert("Multiplication of "+num1+ " and "+num2+ " is "+res);
+}
