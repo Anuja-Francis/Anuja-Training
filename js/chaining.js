77fa974908115036f8f483f3778292a727bb2b90
Anuja Francis <anuja.francis@aspiresys.kl>
2025-01-03 10:33:56 +0530

    first commit

---
+++ js/chaining.js
@@ -0,0 +1,17 @@
+new Promise((resolve) => resolve("First"))
+    .then((result) => {
+        console.log(result); 
+        return "Second"; 
+    })
+    .then((secondResult) => {
+        console.log(secondResult); 
+    });
+
+new Promise((resolve) => resolve("First"))
+    .then((result) => {
+        console.log(result);
+        return new Promise((resolve) => setTimeout(resolve, 1000, "Second"));
+    })
+    .then((secondResult) => {
+        console.log(secondResult); 
+    });
---
+++ js/prototype.js
@@ -0,0 +1,15 @@
+const person = {
+    name: 'John',
+    greet: function() {
+      console.log('Hello, ' + this.name);
+    }
+  };
+  
+  const employee = Object.create(person);
+  employee.job = 'Developer';
+
+  const objName = {};
+  
+  employee.greet();
+  employee.greet.call(objName);
+  console.log(employee.job); 
