d780755a8151ec438cf2ccc22a14f6b7091d1eca
Anuja Francis <anuja.francis@aspiresys.kl>
2025-01-03 11:12:55 +0530

    first commit

---
+++ js/shadowprototype.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'John',
+    greet: function() {
+      console.log('Hello, ' + this.name);
+    }
+};
+
+const employee = Object.create(person);
+employee.greet(); 
+employee.name = 'Alice'; 
+employee.job = 'Developer';
+
+employee.greet(); 
+console.log(employee.job);  
