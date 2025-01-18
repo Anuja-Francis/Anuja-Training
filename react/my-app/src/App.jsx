cb764aac46bb0376adf17ec51a525cb1af017b36
Anuja Francis <anuja.francis@aspiresys.kl>
2025-01-18 15:06:37 +0530

    commit

--- react/my-app/src/App.jsx
+++ react/my-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 import Game from './tic-tac-tau.jsx';
-// import AboutPage from './about.jsx';
 import {ShoppingList,AboutPage} from './about.jsx';
+import Board from './props.jsx';
+import { VideoPlayer } from './useeffect.jsx';
+import App from './thinking.jsx';
+import { Parent } from './lifting.jsx';
 //import logo from './logo.svg';
 
 function MyButton() {
   const [count, setCount] = useState(0); 
+  const [isPlaying, setIsPlaying] = useState(false); 
 
   function handleClick() {
     setCount(count + 1); 
@@ -21,6 +25,16 @@ function MyButton() {
       <AboutPage/>
       <Game/>
       <ShoppingList />
+      <Board/>
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Pause Video' : 'Play Video'}
+      </button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
+      <App/>
+      <Parent/>
     </div>
   );
 }
