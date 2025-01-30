13a6603781617586d19ff624a7c8489895fa3a31
Anuja Francis <anuja.francis@aspiresys.kl>
2025-01-30 09:04:46 +0530

    Second commit

--- react/vite-react/sample-react-app/redux/index.js
+++ react/vite-react/sample-react-app/redux/index.js
@@ -1,34 +1,38 @@
-const redux = require('redux')
-const createStore = redux.createStore
+const { configureStore } = require('@reduxjs/toolkit');
 
-const BUY_CAKE = 'BUY_CAKE'
+const BUY_CAKE = 'BUY_CAKE';
 
-function buyCake(){
+function buyCake() {
     return {
-            type: BUY_CAKE,
-            info: 'First redux action'
-    }
+        type: BUY_CAKE,
+        info: 'First redux action'
+    };
 }
 
 const initialState = {
     numOfCakes: 10
-}
+};
 
 const reducer = (state = initialState, action) => {
-    switch(action.type){
-        case BUY_CAKE: return{
-            ...state,
-            numOfCakes: state.numOfCakes - 1
-        }
+    switch(action.type) {
+        case BUY_CAKE:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes - 1
+            };
 
-        default: return state
+        default:
+            return state;
     }
-}
+};
 
-const store = createStore(reducer)
-console.log('initialState', store.getState())
-const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-store.dispatch(buyCake())
-unsubscribe()
+const store = configureStore({ reducer });
+console.log('initialState', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()));
+
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
